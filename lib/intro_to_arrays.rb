a716269bdf5a3f93378def367ec3e05372aa40e4
Samantha Wong <smiwong920@gmail.com>
2019-02-01 20:33:47 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,3 +10,15 @@ def first_element(array)
   array[0]
 end
 
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def first_element_with_array_methods(array)
+  array.unshift
+end
+
